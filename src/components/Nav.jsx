43be705133cc6f8191e931a13612bed2f56ff127
carvalhaus <ooliveira.joaop@gmail.com>
2024-04-18 19:04:42 -0300

    feat: add animation in icons and create function to handle it

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,51 +8,92 @@ import pokedex_off from "/pokedex-off.svg";
 import pokedex_on from "/pokedex-on.svg";
 import regions_off from "/regions-off.svg";
 import regions_on from "/regions-on.svg";
+import { useEffect, useState } from "react";
 
 function Nav({ url }) {
-  console.log(url);
-
-  let icons = {
+  const [animate, setAnimate] = useState(false);
+  const [icons, setIcons] = useState({
     pokedex_icon: pokedex_on,
     regions_icon: regions_off,
     favorites_icon: favorites_off,
     menu_icon: menu_off,
-  };
+  });
 
-  switch (url.pathname) {
-    case "/regions":
-      icons.pokedex_icon = pokedex_off;
-      icons.regions_icon = regions_on;
-      break;
-    case "/favorites":
-      icons.pokedex_icon = pokedex_off;
-      icons.favorites_icon = favorites_on;
-      break;
-    case "/menu":
-      icons.pokedex_icon = pokedex_off;
-      icons.menu_icon = menu_on;
-      break;
-    default:
-      break;
-  }
+  useEffect(() => {
+    handleUptadeIcons(url);
+
+    const timeoutId = setTimeout(() => {
+      setAnimate(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [url]);
+
+  const handleUptadeIcons = (params) => {
+    const updatedIcons = { ...icons };
+
+    switch (params.pathname) {
+      case "/regions":
+        updatedIcons.pokedex_icon = pokedex_off;
+        updatedIcons.regions_icon = regions_on;
+        updatedIcons.favorites_icon = favorites_off;
+        updatedIcons.menu_icon = menu_off;
+        break;
+      case "/favorites":
+        updatedIcons.pokedex_icon = pokedex_off;
+        updatedIcons.regions_icon = regions_off;
+        updatedIcons.favorites_icon = favorites_on;
+        updatedIcons.menu_icon = menu_off;
+        break;
+      case "/menu":
+        updatedIcons.pokedex_icon = pokedex_off;
+        updatedIcons.regions_icon = regions_off;
+        updatedIcons.favorites_icon = favorites_off;
+        updatedIcons.menu_icon = menu_on;
+        break;
+      default:
+        updatedIcons.pokedex_icon = pokedex_on;
+        updatedIcons.regions_icon = regions_off;
+        updatedIcons.favorites_icon = favorites_off;
+        updatedIcons.menu_icon = menu_off;
+        break;
+    }
+
+    setIcons(updatedIcons);
+    setAnimate(true);
+  };
 
   return (
     <nav className="fixed bottom-0 h-20 flex align-middle justify-center w-full border-t-2 border-gray-300 gap-2 px-8 py-1">
       <Link to={"/"} className="content-center">
-        <img alt="Pokedex icon" src={icons.pokedex_icon} className="h-full" />
+        <img
+          alt="Pokedex icon"
+          src={icons.pokedex_icon}
+          className={`h-full ${animate ? "animate-fadeInUp_animantion" : ""}`}
+        />
       </Link>
       <Link to={"/regions"} className="content-center">
-        <img alt="Location icon" src={icons.regions_icon} className="h-full" />
+        <img
+          alt="Location icon"
+          src={icons.regions_icon}
+          className={`h-full ${animate ? "animate-fadeInUp_animantion" : ""}`}
+        />
       </Link>
       <Link to={"/favorites"} className="content-center">
         <img
           alt="Favorite icon"
           src={icons.favorites_icon}
-          className="h-full"
+          className={`h-full ${animate ? "animate-fadeInUp_animantion" : ""}`}
         />
       </Link>
       <Link to={"/menu"} className="content-center">
-        <img alt="User menu icon" src={icons.menu_icon} className="h-full" />
+        <img
+          alt="User menu icon"
+          src={icons.menu_icon}
+          className={`h-full ${animate ? "animate-fadeInUp_animantion" : ""}`}
+        />
       </Link>
     </nav>
   );
