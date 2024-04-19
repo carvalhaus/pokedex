d6219c9b2ba5ad2357a1c3dbc368adbd97e4b1eb
carvalhaus <ooliveira.joaop@gmail.com>
2024-04-19 08:15:37 -0300

    feat: edit Home component adding input and select

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,52 @@
-import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 function Home() {
   return (
-    <>
-      <h1>HOME</h1>
-      <Button />
-    </>
+    <div className="flex flex-col  gap-6 p-6">
+      <Input type="search" placeholder="Procurar Pokémon" />
+
+      <div className="flex gap-4 justify-between">
+        <Select>
+          <SelectTrigger className="w-[140px]">
+            <SelectValue placeholder="Todos os tipos" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Selecione o tipo</SelectLabel>
+              <SelectItem value="apple">Apple</SelectItem>
+              <SelectItem value="banana">Banana</SelectItem>
+              <SelectItem value="blueberry">Blueberry</SelectItem>
+              <SelectItem value="grapes">Grapes</SelectItem>
+              <SelectItem value="pineapple">Pineapple</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select>
+          <SelectTrigger className="w-[160px]">
+            <SelectValue placeholder="Ordenar por" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Selecione a ordem</SelectLabel>
+              <SelectItem value="lower_number">Menor número</SelectItem>
+              <SelectItem value="highest_number">Maior número</SelectItem>
+              <SelectItem value="a_to_z">A-Z</SelectItem>
+              <SelectItem value="z_to_a">Z-A</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
   );
 }
 
