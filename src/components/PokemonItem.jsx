7c910d07c26c2140435a43514eb9918a8fea4ce3
carvalhaus <ooliveira.joaop@gmail.com>
2024-04-22 19:33:01 -0300

    feat: filter pokemonTypes based on first and second type of pokemon

--- src/components/PokemonItem.jsx
+++ src/components/PokemonItem.jsx
@@ -13,34 +13,55 @@ import fav_off from "../assets/icons/fav-off.svg";
 
 import teste from "../assets/elements_images/normal.svg";
 
-function PokemonItem({ name, id, sprites, types, color }) {
+import { pokemonsTypes } from "@/utils/pokemonsTypes";
+
+function PokemonItem({ name, id, sprites, types }) {
   const Print = () => {
     console.log("CLICKED");
   };
 
   const capitalizedName = name[0].toUpperCase() + name.slice(1);
 
-  const typesNames = types.map((type) => type.type.name);
+  const firstPokemonType = pokemonsTypes.filter(
+    (element) => element.type === types[0].type.name
+  );
+
+  const secondPokemonType = pokemonsTypes.filter(
+    (element) => types.length > 1 && element.type === types[1].type.name
+  );
 
   return (
     <li className="list-none w-80 max-[430px]:w-full">
       <Card
-        className={`flex justify-between overflow-hidden h-28 bg-[${color}]/[0.15]`}
+        style={{ backgroundColor: `rgba(${firstPokemonType[0].color}, 0.15)` }}
+        className={`flex justify-between overflow-hidden h-28`}
       >
         <CardHeader className="px-4 py-3 flex justify-between max-[340px]:max-w-32 overflow-x-auto no-scrollbar">
           <CardDescription className="font-bold">Nº {id}</CardDescription>
           <CardTitle className="truncate h-7">{capitalizedName}</CardTitle>
           <div className="flex gap-1 m-0">
             {types.map((type, index) => (
-              <Badge key={index} className="mt-0">
+              <Badge
+                key={index}
+                className="mt-0"
+                style={{
+                  backgroundColor: `rgba(${color}, 0.15)`,
+                }}
+              >
                 {type.type.name}
               </Badge>
             ))}
           </div>
         </CardHeader>
         <CardContent className="p-0 relative overflow-hidden">
-          <div className={`w-32 h-full bg-[${color}] rounded-lg relative`}>
-            <img src={teste} className="absolute top-0 left-0 my-2 mx-[17px]" />
+          <div
+            style={{ backgroundColor: `rgb(${firstPokemonType[0].color})` }}
+            className={`w-32 h-full rounded-lg relative`}
+          >
+            <img
+              src={firstPokemonType[0].src}
+              className="absolute top-0 left-0 my-2 mx-[17px]"
+            />
             <img
               alt="Pokemon image"
               src={sprites.front_default}
