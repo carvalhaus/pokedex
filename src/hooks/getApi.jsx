76029f9a70cbc64c66aada7a68fe199f1edc0706
João Pedro Carvalho Oliveira <jdeca@DGEO-89RH.1cgeo.dc>
2024-04-25 10:44:59 -0300

    feat: add dependency in useEffect hook to url

--- src/hooks/getApi.jsx
+++ src/hooks/getApi.jsx
@@ -2,26 +2,29 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function getApi(url) {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const fetchData = async () => {
     setLoading(true);
+    setError(false);
 
     try {
       const response = await axios.get(url);
       setData(response.data);
     } catch (error) {
       console.error("Error fetching objects:", error);
+      setError(true);
     }
     setLoading(false);
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [url]);
 
-  return { data, loading };
+  return { data, loading, error };
 }
 
 export default getApi;
