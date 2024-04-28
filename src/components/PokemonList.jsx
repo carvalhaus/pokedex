520cd7049f8ec71b375ef2f02d16c5c171703d95
carvalhaus <ooliveira.joaop@gmail.com>
2024-04-28 17:24:13 -0300

    fix remove unnecessary code and change the way receive data from API

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,24 +1,42 @@
 import { useApi } from "@/context/ApiContext";
 import PokemonItem from "./PokemonItem";
-import Loading from "./Loading";
+
 import Error from "./Error";
+import loading_ellipse from ".././assets/loading-ellipse.svg";
+import { Button } from "./ui/button";
 
 function PokemonList() {
-  const { pokemonsList, loading, error } = useApi();
+  const { pokemons, loading, morePokemons, error } = useApi();
 
-  if (loading) {
-    return <Loading />;
-  }
+  // if (loading) {
+  //   return <Loading />;
+  // }
 
-  if (error) {
-    return <Error />;
-  }
+  const handleMorePokemons = () => {
+    morePokemons();
+  };
 
   return (
     <ul className="flex flex-col justify-center items-center gap-3 sm:flex-row sm:flex-wrap max-[430px]:w-full">
-      {pokemonsList?.map((pokemon, index) => (
-        <PokemonItem key={index} param={pokemon.url} />
-      ))}
+      {error ? (
+        <Error />
+      ) : (
+        <>
+          {pokemons.map((pokemon) => (
+            <PokemonItem
+              key={pokemon.id}
+              name={pokemon.name}
+              types={pokemon.types}
+              sprites={pokemon.sprites}
+              id={pokemon.id}
+            />
+          ))}
+        </>
+      )}
+      <Button onClick={handleMorePokemons}>teste</Button>
+      <li className="h-12 w-full flex justify-center bg-red-50 pt-[-20px]">
+        {loading && <img className="animate-spin" src={loading_ellipse} />}
+      </li>
     </ul>
   );
 }
--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,6 +1,5 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import getApi from "@/hooks/getApi";
-import useScreenSize from "@/hooks/useScreenSize";
+import usePokemons from "@/hooks/usePokemons";
+import { createContext, useContext } from "react";
 
 const ApiContext = createContext();
 
@@ -9,24 +8,10 @@ export const useApi = () => {
 };
 
 function ApiProvider({ children }) {
-  const { width } = useScreenSize();
-
-  let limit = 6;
-
-  if (width > 680) {
-    limit = 30;
-  }
-
-  const { data, loading, error } = getApi(
-    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`
-  );
-
-  const dataResults = [data?.results];
-
-  const pokemonsList = dataResults[0];
+  const { pokemons, error, morePokemons, loading } = usePokemons();
 
   return (
-    <ApiContext.Provider value={{ pokemonsList, loading, error }}>
+    <ApiContext.Provider value={{ pokemons, loading, morePokemons, error }}>
       {children}
     </ApiContext.Provider>
   );
