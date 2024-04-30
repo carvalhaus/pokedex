cdac7646f8d241c8f88a83089ead81c77bf0fcc5
carvalhaus <ooliveira.joaop@gmail.com>
2024-04-30 18:48:59 -0300

    feat: filter and sort PokemonsList

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -4,13 +4,50 @@ import PokemonItem from "./PokemonItem";
 import Error from "./Error";
 import loading_ellipse from ".././assets/loading-ellipse.svg";
 import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
 
-function PokemonList() {
+function PokemonList({ type, sort }) {
   const { pokemons, loading, morePokemons, error } = useApi();
+  const [sortedPokemons, setSortedPokemons] = useState([]);
 
   const handleMorePokemons = () => {
     morePokemons();
   };
+
+  const filterByType = (pokemon) => {
+    const filtered =
+      pokemon.types[0].type.name === type ||
+      pokemon.types[1]?.type.name === type;
+
+    return filtered;
+  };
+
+  const handleSort = () => {
+    const sorted = [...pokemons];
+    switch (sort) {
+      case "highest_number":
+        sorted.sort((pokemonA, pokemonB) => pokemonB.id - pokemonA.id);
+        break;
+      case "a_to_z":
+        sorted.sort((pokemonA, pokemonB) =>
+          pokemonA.name.localeCompare(pokemonB.name)
+        );
+        break;
+      case "z_to_a":
+        sorted.sort((pokemonA, pokemonB) =>
+          pokemonB.name.localeCompare(pokemonA.name)
+        );
+        break;
+      default:
+        break;
+    }
+
+    setSortedPokemons(sorted);
+  };
+
+  useEffect(() => {
+    handleSort();
+  }, [pokemons, sort]);
 
   return (
     <div className="flex flex-col justify-center items-center">
@@ -19,15 +56,27 @@ function PokemonList() {
           <Error />
         ) : (
           <>
-            {pokemons.map((pokemon) => (
-              <PokemonItem
-                key={pokemon.id}
-                name={pokemon.name}
-                types={pokemon.types}
-                sprites={pokemon.sprites}
-                id={pokemon.id}
-              />
-            ))}
+            {!type
+              ? sortedPokemons.map((pokemon) => (
+                  <PokemonItem
+                    key={pokemon.id}
+                    name={pokemon.name}
+                    types={pokemon.types}
+                    sprites={pokemon.sprites}
+                    id={pokemon.id}
+                  />
+                ))
+              : sortedPokemons
+                  .filter(filterByType)
+                  .map((filteredPokemon) => (
+                    <PokemonItem
+                      key={filteredPokemon.id}
+                      name={filteredPokemon.name}
+                      types={filteredPokemon.types}
+                      sprites={filteredPokemon.sprites}
+                      id={filteredPokemon.id}
+                    />
+                  ))}
           </>
         )}
       </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,12 @@ import {
 } from "@/components/ui/select";
 
 import { pokemonsTypes } from "@/utils/pokemonsTypes";
+import { useState } from "react";
 
 function Home() {
+  const [type, setTypes] = useState("");
+  const [sort, setSort] = useState("lower_number");
+
   return (
     <div className="flex flex-col gap-6 p-6 justify-center items-center ">
       <header className="flex flex-col gap-6 w-full sm:w-4/5">
@@ -22,13 +26,20 @@ function Home() {
           className="focus-visible:ring-transparent"
         />
         <div className="flex gap-1 justify-between sm:justify-center sm:gap-6">
-          <Select>
+          <Select
+            onValueChange={(value) => {
+              setTypes(value);
+            }}
+          >
             <SelectTrigger className="w-[140px] focus-visible:ring-transparent">
               <SelectValue placeholder="Todos os tipos" />
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
                 <SelectLabel>Selecione o tipo</SelectLabel>
+                <SelectItem key="null" value={null}>
+                  Default
+                </SelectItem>
                 {pokemonsTypes.map((item) => (
                   <SelectItem key={item.type} value={item.type}>
                     {item.type_pt}
@@ -37,7 +48,11 @@ function Home() {
               </SelectGroup>
             </SelectContent>
           </Select>
-          <Select>
+          <Select
+            onValueChange={(value) => {
+              setSort(value);
+            }}
+          >
             <SelectTrigger className="w-[160px] focus-visible:ring-transparent">
               <SelectValue placeholder="Ordenar por" />
             </SelectTrigger>
@@ -53,7 +68,7 @@ function Home() {
           </Select>
         </div>
       </header>
-      <PokemonList />
+      <PokemonList type={type} sort={sort} />
     </div>
   );
 }
