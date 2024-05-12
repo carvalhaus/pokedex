ba4530f120d370c826bf21daf4e03be1bc2e061d
carvalhaus <ooliveira.joaop@gmail.com>
2024-05-12 18:26:29 -0300

    feat: add Link in PokemonItem to id and fix toggle position

--- src/components/PokemonItem.jsx
+++ src/components/PokemonItem.jsx
@@ -13,7 +13,7 @@ import fav_off from "../assets/icons/fav-off.svg";
 
 import { pokemonsTypes } from "@/utils/pokemonsTypes";
 
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function PokemonItem({
   pokemon = {},
@@ -40,61 +40,63 @@ function PokemonItem({
   location === "favorites" && setFavorite(true);
 
   return (
-    <li className="list-none w-80 max-[430px]:w-full">
-      <Card
-        style={{ backgroundColor: `rgba(${pokemonType[0].color}, 0.15)` }}
-        className={`flex justify-between overflow-hidden h-28`}
-      >
-        <CardHeader className="px-4 py-3 flex justify-between max-[340px]:max-w-32 overflow-x-auto no-scrollbar">
-          <CardDescription className="font-bold">Nº {id}</CardDescription>
-          <CardTitle className="truncate h-7">{capitalizedName}</CardTitle>
-          <div className="flex gap-1 m-0">
-            {pokemonType?.map((type, index) => (
-              <Badge
-                key={index}
-                className="mt-0"
-                style={{
-                  backgroundColor: `rgba(${type.color}, 1)`,
-                }}
-              >
-                {type.type_pt}
-              </Badge>
-            ))}
-          </div>
-        </CardHeader>
+    <li className="list-none w-80 max-[430px]:w-full relative">
+      <Link to={`/pokemon/${id}`}>
+        <Card
+          style={{ backgroundColor: `rgba(${pokemonType[0].color}, 0.15)` }}
+          className={`flex justify-between overflow-hidden h-28`}
+        >
+          <CardHeader className="px-4 py-3 flex justify-between max-[340px]:max-w-32 overflow-x-auto no-scrollbar">
+            <CardDescription className="font-bold">Nº {id}</CardDescription>
+            <CardTitle className="truncate h-7">{capitalizedName}</CardTitle>
+            <div className="flex gap-1 m-0">
+              {pokemonType?.map((type, index) => (
+                <Badge
+                  key={index}
+                  className="mt-0"
+                  style={{
+                    backgroundColor: `rgba(${type.color}, 1)`,
+                  }}
+                >
+                  {type.type_pt}
+                </Badge>
+              ))}
+            </div>
+          </CardHeader>
+          <CardContent className="p-0 relative overflow-hidden">
+            <div
+              style={{ backgroundColor: `rgb(${pokemonType[0].color})` }}
+              className={`w-32 h-full rounded-lg relative`}
+            >
+              <img
+                src={pokemonType[0].src}
+                className="absolute top-0 left-0 my-2 mx-[17px]"
+              />
+              <img
+                alt="Pokemon image"
+                src={sprites.front_default}
+                className="absolute top-0 left-0 my-[7px] mx-4"
+              />
+            </div>
+          </CardContent>
+        </Card>
+      </Link>
 
-        <CardContent className="p-0 relative overflow-hidden">
-          <div
-            style={{ backgroundColor: `rgb(${pokemonType[0].color})` }}
-            className={`w-32 h-full rounded-lg relative`}
-          >
-            <img
-              src={pokemonType[0].src}
-              className="absolute top-0 left-0 my-2 mx-[17px]"
-            />
-            <img
-              alt="Pokemon image"
-              src={sprites.front_default}
-              className="absolute top-0 left-0 my-[7px] mx-4"
-            />
-          </div>
-          <Toggle className="absolute right-1 top-1 p-0 h-8 data-[state=on]:bg-transparent hover:bg-transparent">
-            {isFavorited ? (
-              <img
-                alt="Icon of favorite filled"
-                src={fav_on}
-                onClick={() => removeFavorite(id)}
-              />
-            ) : (
-              <img
-                alt="Icon of favorite outlined"
-                src={fav_off}
-                onClick={() => addFavorite(pokemon)}
-              />
-            )}
-          </Toggle>
-        </CardContent>
-      </Card>
+      <Toggle className="absolute right-1 top-1 p-0 h-8 data-[state=on]:bg-transparent hover:bg-transparent">
+        {isFavorited ? (
+          <img
+            alt="Icon of favorite filled"
+            src={fav_on}
+            onClick={() => removeFavorite(id)}
+          />
+        ) : (
+          <img
+            alt="Icon of favorite outlined"
+            src={fav_off}
+            onClick={() => addFavorite(pokemon)}
+          />
+        )}
+      </Toggle>
     </li>
   );
 }
